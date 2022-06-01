316c3f583e16f373a0453e045883849f5095a898
Hyoban <hyoban@qq.com>
2022-06-01 22:57:41 +0800

    chore: init

---
+++ src/main/java/cc/hyoban/fastspring/FastSpringApplication.java
@@ -0,0 +1,13 @@
+package cc.hyoban.fastspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FastSpringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/cc/hyoban/fastspring/FastSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.hyoban.fastspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
